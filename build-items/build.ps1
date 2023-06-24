2cc5b8e7123194e577a27554da8b731de8a76f13
GreenVenator <sneakyplan@hotmail.co.uk>
2023-06-24 09:45:19 +0100

    Remove old js build

--- build-items/build.ps1
+++ build-items/build.ps1
@@ -1,6 +1,8 @@
-$jsPath = "build-items/build-bookmarklet.js";
-mkdir -p out;
-gci | ? {$_.Name -match '.js$'} | % { 
+If (!(test-path out))
+{
+    mkdir -p out;
+}
+gci | ? {$_.Name -match '\.js$'} | % { 
     $content = Get-Content $_ -Raw;
     $encodedContent = [uri]::EscapeDataString("(function(){$($content.trim())})();");
     $js = "javascript:${encodedContent}";
